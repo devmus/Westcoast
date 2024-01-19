e729e91ea8ab6b4ebd988237395d742532e30b82
Rasmus <rasmus.wersall@gmail.com>
2024-01-19 17:14:39 +0100

    :sparkles: Populära kurser klar

--- App/AppManager.js
+++ App/AppManager.js
@@ -2,11 +2,11 @@ import Course from '../Models/Course.js';
 import { HttpClient } from '../lib/https.js';
 
 export default class AppManager {
-  async rankCourses() {
+  async listCourses() {
     try {
       const http = new HttpClient();
       const result = await http.get('courses');
-      console.log(result);
+      // console.log(result);
 
       const course = result.map((course) => {
         return new Course(
@@ -24,14 +24,27 @@ export default class AppManager {
         );
       });
 
-      course.sort((a, b) => parseFloat(b.review) - parseFloat(a.review));
-
-      let bestCourses = course.slice(0, 5);
-      console.log(bestCourses);
-
-      return bestCourses;
+      return course;
     } catch (error) {
       throw error;
     }
+  }
+
+  async rankCourses() {
+    const courses = await this.listCourses();
+    courses.sort((a, b) => parseFloat(b.review) - parseFloat(a.review));
+    let limitCourses = courses.slice(0, 5);
+    return limitCourses;
+  }
+
+  async popCourses() {
+    const courses = await this.listCourses();
+    courses.sort(
+      (a, b) =>
+        parseFloat(b.students.student_count) -
+        parseFloat(a.students.student_count)
+    );
+    let limitCourses = courses.slice(0, 5);
+    return limitCourses;
   }
 }
